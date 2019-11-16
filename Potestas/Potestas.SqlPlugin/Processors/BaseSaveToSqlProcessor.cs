0b5a38194d76ef0705dcf39c6e4022dde5445cff
Aliaksandra Hushchanskova <aliaksandra_hushchanskova@epam.com>
2019-11-17 01:00:43 +0300

    Add Task 2. Implement SaveToSqlProcessor.

---
+++ Potestas/Potestas.SqlPlugin/Processors/BaseSaveToSqlProcessor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.SqlClient;
+using Potestas.Configuration;
+using Potestas.Utils;
+
+namespace Potestas.SqlPlugin.Processors
+{
+    public abstract class BaseSaveToSqlProcessor<T> : IEnergyObservationProcessor<T> where T : IEnergyObservation
+    {
+        private readonly string _connectionString;
+
+        protected BaseSaveToSqlProcessor(IConfiguration configuration)
+        {
+            _connectionString = configuration?.GetValue("connectionString") ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public void OnCompleted()
+        {
+
+        }
+
+        public void OnError(Exception error)
+        {
+
+        }
+
+        public void OnNext(T value)
+        {
+            if (Validator.IsGenericTypeNull(value))
+            {
+                throw new ArgumentNullException();
+            }
+            
+            using var connection = new SqlConnection(_connectionString);
+            var sqlCommand = new SqlCommand(this.GetQueryString(value), connection);
+            
+            connection.Open();
+            sqlCommand.ExecuteNonQuery();
+        }
+
+        public string Description => "Saves observations to database";
+
+        protected abstract string GetQueryString(T value);
+    }
+}
---
+++ Potestas/Potestas.SqlPlugin/Processors/SaveFlashObservationToSqlProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using Potestas.Configuration;
+using Potestas.Observations;
+
+namespace Potestas.SqlPlugin.Processors
+{
+    public class SaveFlashObservationToSqlProcessor : BaseSaveToSqlProcessor<FlashObservation>
+    {
+        public SaveFlashObservationToSqlProcessor(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        protected override string GetQueryString(FlashObservation value)
+        {
+            var intensity = value.Intensity.ToString(CultureInfo.InvariantCulture);
+            var durationMs = value.DurationMs.ToString();
+            var observationPoint = $@"geometry::STPointFromText('POINT ({value.ObservationPoint.X.ToString(CultureInfo.InvariantCulture)} 
+                                                                        {value.ObservationPoint.X.ToString(CultureInfo.InvariantCulture)})', 0)";
+            var observationTime = value.ObservationTime.ToString(CultureInfo.InvariantCulture);
+            var estimatedValue = value.EstimatedValue.ToString(CultureInfo.InvariantCulture);
+
+            return $@"INSERT INTO dbo.FlashObservations (Intensity, DurationMs, ObservationPoint, ObservationTime, EstimatedValue)
+                                    VALUES ({intensity}, {durationMs}, {observationPoint}, '{observationTime}', {estimatedValue});";
+        }
+    }
+}
