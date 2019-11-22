ff3afc85e021f1234cd63d135d5e637dc099fb20
Aliaksandra Hushchanskova <aliaksandra_hushchanskova@epam.com>
2019-11-22 19:39:49 +0300

    Add task5 forgotten file

---
+++ Potestas/Potestas.SqlPlugin/Factories/SqlProcessingFactory.cs
@@ -0,0 +1,24 @@
+﻿using Potestas.Configuration;
+using Potestas.Observations;
+using Potestas.SqlPlugin.Analyzers;
+using Potestas.SqlPlugin.Processors;
+using Potestas.SqlPlugin.Storages;
+
+namespace Potestas.SqlPlugin.Factories
+{
+    public class SqlProcessingFactory : IProcessingFactory<FlashObservation>
+    {
+        private readonly IConfiguration _configuration;
+
+        public SqlProcessingFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IEnergyObservationProcessor<FlashObservation> CreateProcessor() => new SaveFlashObservationToSqlProcessor(_configuration);
+
+        public IEnergyObservationStorage<FlashObservation> CreateStorage() => new FlashObservationsSqlStorage(_configuration);
+
+        public IEnergyObservationAnalizer CreateAnalizer() => new SqlAnalyzer(_configuration);
+    }
+}
