2b39bc5df758f42b169713253a3dab8307f00f9d
Aliaksandra Hushchanskova <aliaksandra_hushchanskova@epam.com>
2019-11-24 23:49:46 +0300

    Add Task 4. Implement a storage which saves observations to a database using an ORM.

---
+++ Potestas/Potestas.OrmPlugin/Mappers/FlashObservationMapper.cs
@@ -0,0 +1,27 @@
+﻿using Potestas.Observations;
+using FlashObservationDTO = Potestas.CodeFirst.Entities.FlashObservation;
+
+namespace Potestas.OrmPlugin.Mappers
+{
+    internal static class FlashObservationMapper
+    {
+        internal static FlashObservation ToFlashObservation(this FlashObservationDTO flashObservationDto)
+        {
+            return new FlashObservation(flashObservationDto.Id, flashObservationDto.ObservationPoint.ToCoordinates(),
+                flashObservationDto.Intensity, flashObservationDto.DurationMs, flashObservationDto.ObservationTime);
+        }
+
+        internal static FlashObservationDTO ToFlashObservationDTO(this FlashObservation flashObservation)
+        {
+            return new FlashObservationDTO
+            {
+                Id = flashObservation.Id,
+                DurationMs = flashObservation.DurationMs,
+                EstimatedValue = flashObservation.EstimatedValue,
+                Intensity = flashObservation.Intensity,
+                ObservationPoint = flashObservation.ObservationPoint.ToPoint(),
+                ObservationTime = flashObservation.ObservationTime
+            };
+        }
+    }
+}
