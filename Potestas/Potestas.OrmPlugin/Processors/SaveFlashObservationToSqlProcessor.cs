019e7d8c76f7668d5baabebfbda7ab1ec2bbb276
Aliaksandra Hushchanskova <aliaksandra_hushchanskova@epam.com>
2019-11-24 14:42:03 +0300

    Add Task 1. Implement a processor which saves observations to a database using a micro ORM.

---
+++ Potestas/Potestas.OrmPlugin/Processors/SaveFlashObservationToSqlProcessor.cs
@@ -0,0 +1,15 @@
+﻿using Potestas.Configuration;
+using Potestas.Observations;
+using Potestas.SqlHelper;
+
+namespace Potestas.OrmPlugin.Processors
+{
+    public class SaveFlashObservationToSqlProcessor : BaseSaveToSqlProcessor<FlashObservation>
+    {
+        public SaveFlashObservationToSqlProcessor(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        protected override string GetQueryString(FlashObservation value) => FlashObservationQueries.CreateInsertQuery();
+    }
+}
