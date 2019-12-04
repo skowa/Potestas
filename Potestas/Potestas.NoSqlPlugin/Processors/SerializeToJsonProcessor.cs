8eca24d41bf7f915df08ec5eacabe29070e176d7
Aliaksandra Hushchanskova <aliaksandra_hushchanskova@epam.com>
2019-12-04 22:39:02 +0300

    Add Task 1. Implement processor which serializes energy observations to JSON.

---
+++ Potestas/Potestas.NoSqlPlugin/Processors/SerializeToJsonProcessor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Potestas.NoSqlPlugin.Processors.Serializers;
+using Potestas.Processors;
+
+namespace Potestas.NoSqlPlugin.Processors
+{
+    public class SerializeToJsonProcessor<T> : SerializeProcessor<T>, IDisposable, IEnergyObservationProcessor<T> where T : IEnergyObservation
+    {
+        private readonly JsonWriter _jsonWriter;
+        
+        public SerializeToJsonProcessor() : base(new JsonSerializer<T>())
+        {
+            using var writer = new StreamWriter(Stream, leaveOpen: true);
+            _jsonWriter = new JsonTextWriter(writer);
+        }
+
+        public override void OnCompleted()
+        {
+            this.EndDocument();
+
+            base.OnCompleted();
+        }
+
+        public override void OnError(Exception error)
+        {
+            this.EndDocument();
+
+            base.OnError(error);
+        }
+
+        protected override void SerializeToStream(T value)
+        {
+            if (_jsonWriter.WriteState == WriteState.Start)
+            {
+                _jsonWriter.WriteStartArray();
+            }
+
+            (Serializer as JsonSerializer<T>)?.Serialize(_jsonWriter, value);
+        }
+
+        private void EndDocument()
+        {
+            _jsonWriter.WriteEndArray();
+            _jsonWriter.Flush();
+        }
+    }
+}
---
+++ Potestas/Potestas.NoSqlPlugin/Processors/Serializers/JsonSerializer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Potestas.Processors;
+using Potestas.Utils;
+
+namespace Potestas.NoSqlPlugin.Processors.Serializers
+{
+    public class JsonSerializer<T> : ISerializer<T>
+    {
+        private readonly JsonSerializer _jsonSerializer = new JsonSerializer();
+
+        public void Serialize(Stream stream, T value)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            if (Validator.IsGenericTypeNull(value))
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            using var writer = new StreamWriter(stream, leaveOpen:true);
+            using var jsonWriter = new JsonTextWriter(writer);
+            _jsonSerializer.Serialize(jsonWriter, value);
+            jsonWriter.Flush();
+        }
+
+        public T Deserialize(Stream stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            using var reader = new StreamReader(stream);
+            using var jsonReader = new JsonTextReader(reader);
+            return _jsonSerializer.Deserialize<T>(jsonReader);
+        }
+
+        public void Serialize(JsonWriter jsonWriter, T value)
+        {
+            if (jsonWriter == null)
+            {
+                throw new ArgumentNullException(nameof(jsonWriter));
+            }
+
+            if (Validator.IsGenericTypeNull(value))
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            _jsonSerializer.Serialize(jsonWriter, value);
+            jsonWriter.Flush();
+        }
+
+        public T Deserialize(JsonReader jsonReader)
+        {
+            if (jsonReader == null)
+            {
+                throw new ArgumentException(nameof(jsonReader));
+            }
+
+            return _jsonSerializer.Deserialize<T>(jsonReader);
+        }
+    }
+}
