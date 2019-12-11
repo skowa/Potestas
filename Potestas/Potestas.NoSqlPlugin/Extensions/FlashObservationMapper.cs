e87eab022909f1f5221421348a4460fa097ed39e
Aliaksandra Hushchanskova <aliaksandra_hushchanskova@epam.com>
2019-12-11 10:07:42 +0300

    Add Task 2. Implement a storage for NoSQL database.

---
+++ Potestas/Potestas.NoSqlPlugin/Extensions/FlashObservationMapper.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using Potestas.Observations;
+
+namespace Potestas.NoSqlPlugin.Extensions
+{
+	internal static class FlashObservationMapper
+	{
+		internal static BsonDocument ToBsonDocument(this FlashObservation flashObservation)
+		{
+			return new BsonDocument
+			{
+				["DurationMs"]= flashObservation.DurationMs,
+				["EstimatedValue"] = flashObservation.EstimatedValue,
+				["Intensity"] = flashObservation.Intensity,
+				["ObservationTime"] = flashObservation.ObservationTime,
+				["ObservationPoint"] = flashObservation.ObservationPoint.ToBsonDocument()
+			};
+		}
+
+		internal static FlashObservation ToFlashObservation(this BsonDocument bsonDocument)
+		{
+			var coordinatesBsonDocument = bsonDocument["ObservationPoint"] as BsonDocument;
+			var coordinates = coordinatesBsonDocument != null
+				? new Coordinates(coordinatesBsonDocument["X"].AsDouble, coordinatesBsonDocument["Y"].AsDouble)
+				: new Coordinates();
+
+			return new FlashObservation(coordinates, bsonDocument["Intensity"].AsDouble, bsonDocument["DurationMs"].AsInt32, bsonDocument["ObservationTime"].ToUniversalTime());
+		}
+	}
+}
---
+++ Potestas/Potestas.NoSqlPlugin/Storages/FlashObservationNoSqlStorage.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using Potestas.Configuration;
+using Potestas.NoSqlPlugin.Extensions;
+using Potestas.Observations;
+
+namespace Potestas.NoSqlPlugin.Storages
+{
+    public class FlashObservationNoSqlStorage : BaseNoSqlStorage<FlashObservation>
+    {
+        public FlashObservationNoSqlStorage(IConfiguration configuration) : base(configuration)
+        {
+
+        }
+
+        protected override string CollectionName { get; } = "FlashObservations";
+
+		protected override BsonDocument CreateBsonDocument(FlashObservation item)
+		{
+			return item.ToBsonDocument();
+		}
+
+        protected override FlashObservation FromBsonDocument(BsonDocument bsonDocument)
+        {
+	        return bsonDocument.ToFlashObservation();
+        }
+    }
+}
